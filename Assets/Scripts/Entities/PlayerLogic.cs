00663adf0d72d6218f0a7deff5ff2e9f24be0983
Juan Diaz Gutierrez <juan.rod.diaz@gmail.com>
2021-06-30 20:12:29 +0200

    Adding input scripts.

--- Assets/Scripts/Entities/PlayerLogic.cs
+++ Assets/Scripts/Entities/PlayerLogic.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLogic : MonoBehaviour
+public class PlayerLogic : MonoBehaviour, IInputListener
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Subscribe()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
---
+++ Assets/Scripts/Interfaces/IInputListener.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputListener
+{
+    public void Subscribe();
+}
