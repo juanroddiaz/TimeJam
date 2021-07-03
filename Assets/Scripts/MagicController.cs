cb66f8a0fd4e1404e4d5df5300e2ee98f15d6939
Juan Diaz Gutierrez <juan.rod.diaz@gmail.com>
2021-07-03 16:20:55 +0200

    Addign slow and fast magic instances to update.

--- Assets/Scripts/MagicController.cs
+++ Assets/Scripts/MagicController.cs
@@ -2,9 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct MagicData
+{
+    public int Limit;
+    public float Current;
+}
+
+public enum MagicStatus
+{
+    Slow,
+    Fast,
+    Normal,
+}
+
 public class MagicController : MonoBehaviour, IInputListener
 {
-    [SerializeField] private float _defaultMagicDuration = 2.0f;
+    [SerializeField] private float _defaultMagicSpeed = 2.0f;
+    [SerializeField] private MagicData _slowMagicData = default;
+    [SerializeField] private MagicData _fastMagicData = default;
+
+    private MagicStatus _currentStatus = MagicStatus.Normal;
+    private MagicData _currentSlowMagicData = default;
+    private MagicData _currentFastMagicData = default;
+
+    private MagicData CurrentSlowMagic => _currentSlowMagicData;
+    private MagicData CurrentFastMagic => _currentFastMagicData;
 
     private static MagicController _instance;
     public static MagicController Instance
@@ -24,6 +46,8 @@ public class MagicController : MonoBehaviour, IInputListener
     {
         Subscribe();
         DontDestroyOnLoad(gameObject);
+        _currentSlowMagicData = _slowMagicData;
+        _currentFastMagicData = _fastMagicData;
     }
 
     public void Subscribe()
@@ -32,26 +56,70 @@ public class MagicController : MonoBehaviour, IInputListener
         InputController.Instance.FastButtonDown += OnFastMagic;
     }
 
-    private void OnSlowMagic(bool buttonIsDown)
+    void Update()
     {
-        if (buttonIsDown)
+        switch(_currentStatus)
         {
-            TimeController.Instance.TriggerSlowMotion();
-        } else
-        {
-            TimeController.Instance.TriggerNormalMotion();
-        }
+            case MagicStatus.Normal:
+            {
+                break;
+            }
+            case MagicStatus.Slow:
+            {
+                break;
+            }
+            case MagicStatus.Fast:
+            {
+                break;
+            }
+        }            
     }
 
-    private void OnFastMagic(bool buttonIsDown)
+    private void OnSlowMagic(bool toggle)
     {
-        if (buttonIsDown)
+        if(_currentStatus != MagicStatus.Normal && toggle)
+        {
+            Debug.Log("Trying slow magic while not in normal state");
+            return;
+        }
+        if (_currentStatus != MagicStatus.Slow && !toggle)
+        {
+            Debug.Log("Trying to return to normal with no slow magic");
+            return;
+        }
+
+        if(toggle)
+        {
+            TimeController.Instance.TriggerSlowMotion();
+            _currentStatus = MagicStatus.Slow;
+            return;
+        }
+
+        TimeController.Instance.TriggerNormalMotion();
+        _currentStatus = MagicStatus.Normal;
+    }
+
+    private void OnFastMagic(bool toggle)
+    {
+        if (_currentStatus != MagicStatus.Normal && toggle)
+        {
+            Debug.Log("Trying fast magic while not in normal state");
+            return;
+        }
+        if (_currentStatus != MagicStatus.Fast && !toggle)
+        {
+            Debug.Log("Trying to return to normal with no fast magic");
+            return;
+        }
+
+        if (toggle)
         {
             TimeController.Instance.TriggerFastMotion();
+            _currentStatus = MagicStatus.Fast;
+            return;
         }
-        else
-        {
-            TimeController.Instance.TriggerNormalMotion();
-        }
+
+        TimeController.Instance.TriggerNormalMotion();
+        _currentStatus = MagicStatus.Normal;
     }
 }
